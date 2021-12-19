a9b55f6e05f9be6abb70dbb13aa4a635060230ba
Samk13 <samarbid13@gmail.com>
2021-12-20 00:47:12 +0100

    contextmaneger example

--- contextmaneger_example.py
+++ contextmaneger_example.py
@@ -19,6 +19,14 @@ def change_dir(path):
 
 
 with change_dir("D:\Workspace\code\Python"):
-    print(os.listdir())
-with change_dir("D:\Workspace\code"):
-    print(os.listdir())
+    first_dir = os.listdir()
+    # print(os.listdir())
+# with change_dir("D:\Workspace\code"):
+#     second_dir = os.listdir()
+#     print(os.listdir())
+
+for index, folder in enumerate(first_dir, start=1):
+    print(index, folder)
+
+# for index, folder in enumerate(second_dir):
+#     print(index, folder)
