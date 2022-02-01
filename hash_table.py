59a3d89ed12c1447c90bb0ca0eb50b60f26c00ec
Samk13 <samarbid13@gmail.com>
2022-02-01 23:57:18 +0100

    add more get multi function

--- hash_table.py
+++ hash_table.py
@@ -63,3 +63,36 @@ class HashTable:
             else:
                 prev.next = node.next.next
             return result
+
+    def get_all_keys(self):
+        result = []
+        for bucket in self.buckets:
+            node = bucket
+            while node is not None:
+                result.append((node.key, node.value))
+                node = node.next
+        return result
+
+    def insert_multiple(self, keys, values):
+        for key, value in zip(keys, values):
+            self.insert(key, value)
+
+
+x = HashTable()
+x.insert('123123', 1)
+x.insert('b', 2)
+x.insert('c', 3)
+x.insert('d', 4)
+x.insert('e', 5)
+x.insert('f', 6)
+x.insert('g', 7)
+x.insert('h', 8)
+x.insert('i', 9)
+x.insert('j', 10)
+x.insert('k', 11)
+x.insert('l', 12)
+x.insert('m', 13)
+x.insert_multiple(['n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'], [
+                  14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26])
+print(x.find('a'))
+print(x.get_all_keys())
